707cb3c95c7f4020027ece8132e84414ca112664
Abolfazl Haghdoost <abolfazl.ha.0221@gmail.com>
2025-04-06 17:00:12 +0330

    Search box 1 step
    build from base
    project for filter data and
    search for name , last name , email...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Users } from "./Data/user";
+import Table from "./Components/Table";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const keys = ["first_name", "last_name", "email"];
+  console.log(Users);
+
+  const search = (data) => {
+    return data.filter((item) =>
+       keys.some((key) => item[key].includes(query)));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app flex flex-col bg-teal-300/80 space-y-4 center border rounded-sm w-1/2 p-4 m-2 mx-auto">
+      <h2 className="mx-auto">Search box mini project</h2>
+      <div className="mx-auto">
+        <input
+          type="text"
+          placeholder="Search Contact..."
+          className="search text-center border rounded-md shadow-md"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </div>
+      <Table data={search(Users)} />
     </div>
   );
 }
